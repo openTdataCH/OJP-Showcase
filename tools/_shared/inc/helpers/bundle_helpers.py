8c3e6c55a1e578e4aed9adac5b790460fd5dd9f8
Vasile Cotovanu <vasile.cotovanu@gmail.com>
2021-04-14 08:32:01 +0200

    Bundle helpers to load project assets

---
+++ tools/_shared/inc/helpers/bundle_helpers.py
@@ -0,0 +1,19 @@
+import os, sys
+from pathlib import Path
+
+def load_resource_from_bundle(map_resource_paths: dict, resource_key: str):
+    if not resource_key in map_resource_paths:
+        print(f'ERROR: cant find {resource_key} in map')
+        print(map_resource_paths)
+        sys.exit(1)
+
+    resource_path = Path(map_resource_paths[resource_key])
+    if not os.path.isfile(resource_path):
+        print(f"ERROR: cant find path \n{resource_path}")
+        sys.exit(1)
+
+    resource_file_handler = open(resource_path)
+    resource_content = resource_file_handler.read()
+    resource_file_handler.close()
+
+    return resource_content
