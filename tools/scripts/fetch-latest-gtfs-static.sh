27c009fa3f10daf600e114400799c62df056463f
Vasile Cotovanu <vasile.cotovanu@gmail.com>
2021-09-29 11:11:09 +0200

    Adds full path for .ENV

--- tools/scripts/fetch-latest-gtfs-static.sh
+++ tools/scripts/fetch-latest-gtfs-static.sh
@@ -1,8 +1,8 @@
-set -a
-source .ENV.LOCAL
-set +a
-
 SCRIPT_PATH="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
+
+set -a
+source $SCRIPT_PATH/.ENV.LOCAL
+set +a
 
 tool_path=$SCRIPT_PATH/../gtfs-static-fetch-latest/gtfs_static_fetch_latest_cli.py
 
