996bb9685958bebe5e5c759f5695aec561ef0202
Vasile Cotovanu <vasile.cotovanu@gmail.com>
2021-10-10 23:21:42 +0200

    No more need of this

--- tools/gtfs-static-db-importer/inc/sql/update_trips_times.sql
+++
@@ -1,9 +0,0 @@
-UPDATE trips 
-SET 
-    departure_day_minutes = :departure_day_minutes, 
-    arrival_day_minutes = :arrival_day_minutes, 
-    departure_time = :departure_time, 
-    arrival_time = :arrival_time, 
-    stop_times_s = :stop_times_s 
-WHERE 
-    trip_id = :trip_id
