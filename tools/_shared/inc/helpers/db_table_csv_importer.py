b533dd4759e8e4bd0dafcc26213d17918311b42a
Vasile Cotovanu <vasile.cotovanu@gmail.com>
2021-10-10 21:12:45 +0200

    Use different approach for batch INSERTs - commit fast - use executemany for batch updates

--- tools/_shared/inc/helpers/db_table_csv_importer.py
+++ tools/_shared/inc/helpers/db_table_csv_importer.py
@@ -11,19 +11,16 @@ class DB_Table_CSV_Importer:
     def __init__(self, db_path: Path, temp_path: Path, table_name: str, table_config: any):
         self.db_path = db_path
         self.db_handle = sqlite3.connect(db_path)
+        
+        self.db_handle.execute('PRAGMA synchronous = OFF')
+        self.db_handle.execute('PRAGMA journal_mode = OFF')
+        self.db_handle.commit()
+
         self.table_name = table_name
         self.table_config = table_config
-        
-        self.column_names = None
-
-        table_csv_file_path = Path(f'{temp_path}/db-import-{table_name}.csv')
-        if os.path.isfile(table_csv_file_path):
-            os.remove(table_csv_file_path)
-        self.table_csv_file = open(table_csv_file_path, 'w')
 
     def truncate_table(self):
         drop_and_recreate_table(self.db_handle, self.table_name, self.table_config)
-        self.column_names = fetch_column_names(self.db_handle, self.table_name)
 
     def load_csv_file(self, csv_path):
         log_message(f'START LOAD CSV: {csv_path.name}')
@@ -36,53 +33,50 @@ class DB_Table_CSV_Importer:
 
         rows_report_no = self._compute_rows_report_no(lines_no)
 
+        column_names = fetch_column_names(self.db_handle, self.table_name)
+        column_names_s = ', '.join(column_names)
+        values_s = ('?, ' * len(column_names))[0:-2]
+        template_insert_sql = f'INSERT INTO {self.table_name}({column_names_s}) VALUES({values_s})'
+
+        batch_insert_rows_no = 10000
+        batch_insert_values = []
+
+        insert_cursor = self.db_handle.cursor()
+
         for csv_row in csv_reader:
             if csv_row_id % rows_report_no == 0:
                 log_message(f'... parsed {csv_row_id}/{lines_no} rows')
 
-            self._prepare_insert_row(csv_row)
+            if len(batch_insert_values) >= batch_insert_rows_no:
+                insert_cursor.executemany(template_insert_sql, batch_insert_values)
+                self.db_handle.commit()
+                batch_insert_values = []
+
+            row_values = []
+            for key in column_names:
+                field_value = csv_row.get(key, None)
+                row_values.append(field_value)
+            batch_insert_values.append(row_values)
+            
             csv_row_id += 1
 
         csv_import_handler.close()
 
-        self._load_csv_into_db()
-        self._add_table_indexes()
+        insert_cursor.executemany(template_insert_sql, batch_insert_values)
+        self.db_handle.commit()
 
         log_message(f'... DONE  LOAD CSV')
 
     def _compute_rows_report_no(self, rows_no):
         if rows_no < 1000000:
-            return 100000
+            return 500000
         
-        return 500000
+        return 1000000
 
-    def _prepare_insert_row(self, row_values_dict: any):
-        if not self.column_names:
-            self.column_names = fetch_column_names(self.db_handle, self.table_name)
+    def close(self):
+        self.db_handle.close()
 
-        writer = csv.DictWriter(self.table_csv_file, fieldnames=self.column_names)
-        writer.writerow(row_values_dict)
-
-    def _load_csv_into_db(self):
-        # the CSV handler needs to be closed before being used by sqlite3
-        self.table_csv_file.close()
-
-        csv_file_path = self.table_csv_file.name
-        sql_file_path = f'{self.table_csv_file.name}.sql'
-
-        sql_file_rows = [
-            ".separator ','",
-            f'.import "{csv_file_path}" {self.table_name}',
-        ]
-
-        sql_file = open(sql_file_path, 'w')
-        sql_file.write("\n".join(sql_file_rows))
-        sql_file.close()
-
-        shell_import = f'sqlite3 "{self.db_path}" < "{sql_file_path}"'
-        log_message(f'... START import into {self.table_name} table')
-        os.system(shell_import)
-        log_message('... DONE import')
-
-    def _add_table_indexes(self):
+    def add_table_indexes(self):
+        log_message('... adding indexes')
         add_table_indexes(self.db_handle, self.table_name, self.table_config)
+        log_message('... DONE adding indexes')
