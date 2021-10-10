6f54721357b25987fe94db6fd64c363d6660337c
Vasile Cotovanu <vasile.cotovanu@gmail.com>
2021-10-10 23:20:21 +0200

    More fieldnames

--- tools/gtfs-static-db-importer/inc/sql/select_trips_group_by_stop_times.sql
+++ tools/gtfs-static-db-importer/inc/sql/select_trips_group_by_stop_times.sql
@@ -1,5 +1,10 @@
 SELECT 
     trips.trip_id,
+    trips.route_id,
+    trips.service_id,
+    trips.trip_headsign,
+    trips.trip_short_name,
+    trips.direction_id,
     GROUP_CONCAT(PRINTF('%s|%s|%s|%s', stop_times.ROWID, stop_times.stop_id, stop_times.arrival_time, stop_times.departure_time)) AS stop_times_data
 FROM trips, stop_times
 WHERE
