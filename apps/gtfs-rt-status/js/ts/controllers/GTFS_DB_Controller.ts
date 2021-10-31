3c54d63978f22e027b87b52fcd4ccd663315a940
Vasile Cotovanu <vasile.cotovanu@gmail.com>
2021-10-31 18:29:36 +0100

    Use the API instead of the static snapshot

--- apps/gtfs-rt-status/js/ts/controllers/GTFS_DB_Controller.ts
+++ apps/gtfs-rt-status/js/ts/controllers/GTFS_DB_Controller.ts
@@ -1,10 +1,13 @@
 import Progress_Controller from './Progress_Controller'
 import { Date_Helpers } from './../helpers/Date_Helpers' 
+import { URL_Helpers } from '../helpers/URL_Helpers';
 import GTFS_RT_Reporter from './GTFS_RT_Reporter';
 
 export default class GTFS_DB_Controller {
     public progress_controller: Progress_Controller | null = null;
     public gtfs_rt_reporter: GTFS_RT_Reporter | null = null;
+
+    private gtfs_query_base_address: string;
 
     private gtfs_query_btn: HTMLButtonElement;
 
@@ -32,6 +35,8 @@ export default class GTFS_DB_Controller {
 
         this.gtfs_query_btn.disabled = true;
 
+        this.gtfs_query_base_address = 'https://www.m23.ch/customers/openTdataCH/gtfs-query';
+
         this.update_request_time();
     }
 
@@ -57,25 +62,17 @@ export default class GTFS_DB_Controller {
     public load_resources(completion: () => void) {
         this.progress_controller?.setBusy('Loading Resources...');
 
-        let gtfsDBDay = new Date();
-        
-        const gtfs_db_day_s = this.computeGTFS_DB_Day(gtfsDBDay);
-        
-        let gtfs_db_snapshot_base = 'https://opentdatach.github.io/assets-gtfs-static-snapshot';
-        gtfs_db_snapshot_base += '/gtfs_' + gtfs_db_day_s;
-        
-        const gtfs_db_lookups_url = gtfs_db_snapshot_base + '/db_lookups.json';
-        
-        const request_day_s = this.query_request_day_el.value;
-        let gtfs_db_trips_url = gtfs_db_snapshot_base + '/trips_' + request_day_s + '.json';
-        if (this.use_filtered_lookup) {
-            console.log('WARNING - using filtered lookup');
-            gtfs_db_trips_url = gtfs_db_snapshot_base + '-FILTER/trips_' + request_day_s + '.json';
-        }
+        const date_f = Date_Helpers.formatDateYMDHIS(this.request_datetime);
+
+        const gtfs_query_lookups_qs_params = {
+            day: date_f.substring(0, 10),
+            hhmm: date_f.substring(11, 16).replace(':', ''),
+        };
+        const gtfs_query_lookups_address = this.gtfs_query_base_address + '/db_lookups?' 
+            + URL_Helpers.dict_to_querystring(gtfs_query_lookups_qs_params);
 
         const resource_files = [
-            gtfs_db_lookups_url,
-            gtfs_db_trips_url,
+            gtfs_query_lookups_address,
         ]
 
         Promise.all(resource_files.map( resource_file => fetch(resource_file))).then(responses =>
@@ -87,8 +84,6 @@ export default class GTFS_DB_Controller {
             this.gtfs_rt_reporter?.loadAgency(data_response_lookups.agency);
             this.gtfs_rt_reporter?.loadStops(data_response_lookups.stops);
             this.gtfs_rt_reporter?.loadRoutes(data_response_lookups.routes);
-
-            this.gtfs_rt_reporter?.loadTrips(data_responses[1]);
 
             this.gtfs_query_btn.disabled = false;
             this.progress_controller?.setIdle();
@@ -139,15 +134,29 @@ export default class GTFS_DB_Controller {
     }
 
     private handle_gtfs_query_btn_click() {
-        this.progress_controller?.setBusy('Fetching GTFS-RT ...');
+        this.progress_controller?.setBusy('Fetching GTFS static / RT ...');
         this.gtfs_query_btn.disabled = true;
 
         this.update_request_time();
 
         let gtfs_rt_url = 'https://www.webgis.ro/tmp/proxy-gtfsrt2020/gtfsrt2020';
+        const gtfs_query_active_trips_params = {
+            day: this.query_request_day_el.value,
+            hhmm: this.query_request_time_el.value.replace(':', ''),
+            from_hhmm: this.query_interval_from_time_el.value.replace(':', ''),
+            to_hhmm: this.query_interval_to_time_el.value.replace(':', ''),
+            filter_agency_ids: 'HAS_GTFS_RT',
+            parse_type: 'FLAT',
+        };
+        const gtfs_query_active_trips_address = this.gtfs_query_base_address + '/query_active_trips?' 
+            + URL_Helpers.dict_to_querystring(gtfs_query_active_trips_params);
 
-        const gtfs_rt_promise = fetch(gtfs_rt_url);
-        Promise.all([gtfs_rt_promise]).then(responses =>
+        const resource_files = [
+            gtfs_rt_url,
+            gtfs_query_active_trips_address,
+        ]
+
+        Promise.all(resource_files.map( resource_file => fetch(resource_file))).then(responses =>
             Promise.all(responses.map(response => response.json()))
         ).then(data_responses => {
             this.gtfs_query_btn.disabled = false;
@@ -156,11 +165,13 @@ export default class GTFS_DB_Controller {
             const gtfs_rt_response = data_responses[0];
             this.gtfs_rt_reporter?.setRequestDatetime(this.request_datetime);
 
+            const data_response_active_trips = data_responses[1];
+            this.gtfs_rt_reporter?.loadTrips(data_response_active_trips.rows);
+
             const request_interval_from_hhmm = this.query_interval_from_time_el.value;
             const request_interval_from_date = Date_Helpers.setHHMMToDate(this.request_datetime, request_interval_from_hhmm);
 
             const request_interval_to_hhmm = this.query_interval_to_time_el.value;
-            
             let request_interval_to_date = Date_Helpers.setHHMMToDate(this.request_datetime, request_interval_to_hhmm);
             if (request_interval_to_date < request_interval_from_date) {
                 request_interval_to_date.setDate(request_interval_to_date.getDate() + 1);
