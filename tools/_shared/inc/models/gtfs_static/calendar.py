de9f3b61e7a712de243c30bdebed53dc3119cec4
Vasile Cotovanu <vasile.cotovanu@gmail.com>
2022-01-24 00:15:17 +0100

    Create method that can be reused to generate the array of service days. Again, None is better than empty string.

--- tools/_shared/inc/models/gtfs_static/calendar.py
+++ tools/_shared/inc/models/gtfs_static/calendar.py
@@ -1,3 +1,4 @@
+import sys
 import sqlite3
 from datetime import datetime, timedelta
 
@@ -51,34 +52,47 @@ class Calendar:
         pattern_s = ''.join(pattern_parts)
         return pattern_s
 
-    def _debug_days(self):
+    def compute_month_data_rows(self):
         # First day of Dec
         calendar_day1_s = self.start_date.strftime('%Y-%m-01')
-        current_date = datetime.strptime(calendar_day1_s, "%Y-%m-%d").date()
+        current_day = datetime.strptime(calendar_day1_s, "%Y-%m-%d").date()
 
         month_data_rows = []
-        month_day_bits = []
+        empty_day_bit_rows = []
         prev_month_key = None
 
-        while current_date <= self.end_date:
-            month_key = current_date.strftime('%Y-%m')
+        map_weekday_f = ['Mo', 'Tu', 'We', 'Th', 'Fr', 'Sa', 'Su']
+
+        while current_day <= self.end_date:
+            month_key = current_day.strftime('%Y-%m')
             if prev_month_key != month_key:
-                month_day_bits = [' '] * current_date.weekday()
+                # Padding with None to always start from Monday
+                empty_day_bit_rows = [None] * current_day.weekday()
                 month_data = {
                     'month_key': month_key,
-                    'day_bits': month_day_bits,
+                    'day_bit_rows': empty_day_bit_rows,
                 }
                 month_data_rows.append(month_data)
 
                 prev_month_key = month_key
 
-            day_bit = ' '
-            if current_date >= self.start_date:
-                day_idx = (current_date - self.start_date).days
-                day_bit = self.day_bits[day_idx]
-            month_data['day_bits'].append(day_bit)
+            day_bit = None
+            if current_day >= self.start_date:
+                day_idx = (current_day - self.start_date).days
+                day_bit = self.day_bits[day_idx] == '1'
+            day_bit_row = {
+                'day_f': f'{current_day}',
+                'day_w': map_weekday_f[current_day.weekday()],
+                'day_bit': day_bit,
+            }
+            month_data['day_bit_rows'].append(day_bit_row)
 
-            current_date += timedelta(days=1)
+            current_day += timedelta(days=1)
+
+        return month_data_rows
+
+    def _debug_days(self):
+        month_data_rows = self.compute_month_data_rows()
 
         header_separator_s = '-' * 60
         print(f'{header_separator_s}')
@@ -90,13 +104,20 @@ class Calendar:
 
         for month_data in month_data_rows:
             month_bits_formatted = []
-            for (idx, day_bit) in enumerate(month_data['day_bits']):
-                day_bit_s = '·' if day_bit == '0' else day_bit
+            for (idx, day_bit_data) in enumerate(month_data['day_bit_rows']):
+                day_bit_s = ' '
+                if day_bit_data is not None:
+                    day_bit = day_bit_data['day_bit']
+                    day_bit_s = ' '
+                    if day_bit is not None:
+                        day_bit_s = '1' if day_bit else '·'
                 month_bits_formatted.append(day_bit_s)
                 
+                # space after weekdays
                 if (idx % 7) == 6:
                     month_bits_formatted.append(' ')
             month_key = month_data['month_key']
+
             month_bits_s = ''.join(month_bits_formatted)
             print(f'{month_key} : {month_bits_s}')
 
