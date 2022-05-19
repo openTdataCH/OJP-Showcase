f51a03469dd9fe6635da02a066ed6865d766afb0
Vasile Cotovanu <vasile.cotovanu@gmail.com>
2022-05-19 17:10:56 +0200

    Adds new helper for loading config

--- tools/_shared/inc/helpers/config_helpers.py
+++ tools/_shared/inc/helpers/config_helpers.py
@@ -1,4 +1,4 @@
-import os
+import os, sys
 import yaml
 
 from pathlib import Path
@@ -13,3 +13,17 @@ def load_yaml_config(config_path: Path, app_path: Path):
     config_file_handler.close()
 
     return config
+
+def load_convenience_config(context_path: Path):
+    context_folder_path = context_path
+    if os.path.isfile(context_folder_path):
+        context_folder_path = context_folder_path.parent
+
+    app_config_path = Path(f'{context_folder_path}/inc/config.yml')
+    if not os.path.isfile(app_config_path):
+        print(f'ERROR: cant load config from {app_config_path}')
+        sys.exit(1)
+    
+    app_config = load_yaml_config(app_config_path, context_folder_path)
+
+    return app_config
