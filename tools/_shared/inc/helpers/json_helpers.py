b02a7c9c72cd5c29283ee9db0218f9b5e25d80f0
Vasile Cotovanu <vasile.cotovanu@gmail.com>
2022-05-19 17:14:10 +0200

    Hello JSON helpers

---
+++ tools/_shared/inc/helpers/json_helpers.py
@@ -0,0 +1,30 @@
+import os, sys
+import json
+
+from pathlib import Path
+
+def export_json_to_file(json_obj: any, json_path: Path, pretty_print = False):
+    if isinstance(json_path, str):
+        json_path = Path(json_path)
+
+    if not os.path.isdir(json_path.parent):
+        os.makedirs(json_path.parent)
+
+    json_file = open(json_path, 'w', encoding='utf-8')
+    if pretty_print:
+        json_file.write(json.dumps(json_obj, indent=4, ensure_ascii=False))
+    else:
+        json_file.write(json_obj)
+    
+    json_file.close()
+
+def load_json_from_file(json_path: Path):
+    if isinstance(json_path, str):
+        json_path = Path(json_path)
+
+    json_file = open(json_path, encoding='utf-8')
+    json_obj = json.loads(json_file.read())
+    json_file.close()
+
+    return json_obj
+
