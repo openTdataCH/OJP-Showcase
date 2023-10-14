55515c181c10c9abd2c054d893a9f51df6540551
Vasile Cotovanu <vasile.cotovanu@gmail.com>
2023-10-14 22:50:18 +0200

    app_path can be None so we can load arbitrary configs

--- tools/_shared/inc/helpers/config_helpers.py
+++ tools/_shared/inc/helpers/config_helpers.py
@@ -3,11 +3,14 @@ import yaml
 
 from pathlib import Path
 
-def load_yaml_config(config_path: Path, app_path: Path):
+def load_yaml_config(config_path: Path, app_path: Path = None):
     config_file_handler = open(f'{config_path}', encoding='utf-8')
 
     config_s = config_file_handler.read()
-    config_s = config_s.replace('[APP_PATH]', f'{app_path}')
+    
+    if app_path is not None:
+        config_s = config_s.replace('[APP_PATH]', f'{app_path}')
+    
     config = yaml.safe_load(config_s)
   
     config_file_handler.close()
