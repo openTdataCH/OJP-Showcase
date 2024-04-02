c99762c1cc988b0e66d0ef9e1186e1960ea3ea0f
Vasile Cotovanu <vasile.cotovanu@gmail.com>
2024-04-02 22:36:11 +0200

    Links data service

--- apps/gtfs-rt-static-report/src/app/app.component.ts
+++ apps/gtfs-rt-static-report/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { DataService } from './data.service';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,14 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'gtfs-rt-static-report';
+
+  constructor(private dataService: DataService) {
+
+  }
+
+  ngOnInit() {
+    this.dataService.getMonthlyReport('2024-03').subscribe((response) => {
+      console.log(response);
+    });
+  }
 }
