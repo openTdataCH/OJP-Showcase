e9350fb1a630887357802ce057e838c9233cbf6f
Vasile Cotovanu <vasile.cotovanu@gmail.com>
2024-05-21 21:08:02 +0200

    Adds models

--- apps/gtfs-rt-static-report/src/app/app.component.ts
+++ apps/gtfs-rt-static-report/src/app/app.component.ts
@@ -1,6 +1,26 @@
 import { Component } from '@angular/core';
 import { DataService } from './data.service';
 
+interface GTFS_RT_Static_Monthly_Report {
+  comments: string
+  report_days: Record<string, Record<string, GTFS_RT_Static_Report_Metadata>>
+}
+interface GTFS_RT_Static_Report_Metadata {
+  report_dt: String
+  gtfs_db_filename: String
+  gtfs_db_age: String
+  gtfs_rt_filename: String
+  gtfs_rt_ts: Number
+  gtfs_rt_dt: String
+  gtfs_rt_age: Number
+  total_rows_no: Number
+  tripOK_routeOK_no: Number
+  tripOK_routeNOK_no: Number
+  tripNOK_routeOK_no: Number
+  tripNOK_routeNOK_no: Number
+  tripNOK_NOJP_no: Number
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
