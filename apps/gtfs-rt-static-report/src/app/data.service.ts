b2571d08f7bb22fe64f15d70c32187031d66efb6
Vasile Cotovanu <vasile.cotovanu@gmail.com>
2024-05-22 09:12:12 +0200

    Allow year also

--- apps/gtfs-rt-static-report/src/app/data.service.ts
+++ apps/gtfs-rt-static-report/src/app/data.service.ts
@@ -9,9 +9,13 @@ export class DataService {
 
   constructor(private http: HttpClient) { }
 
-  getMonthlyReport(monthF: string): Observable<any> {
-    let API_ENDPOINT = 'https://tools.odpch.ch/gtfs-rt-static-compare-report/2024/gtfs_rt_static_report-[YYYY-MM].json'
-    API_ENDPOINT = API_ENDPOINT.replace('[YYYY-MM]', monthF);
+  getMonthlyReport(dateYM: string): Observable<any> {
+    const dateYMParts = dateYM.split('-');
+
+    let API_ENDPOINT = 'https://tools.odpch.ch/gtfs-rt-static-compare-report/[YYYY]/gtfs_rt_static_report-[YYYY-MM].json'
+    API_ENDPOINT = API_ENDPOINT.replace('[YYYY]', dateYMParts[0]);
+    API_ENDPOINT = API_ENDPOINT.replace('[YYYY-MM]', dateYM);
+    
     return this.http.get(API_ENDPOINT);
   }
 }
