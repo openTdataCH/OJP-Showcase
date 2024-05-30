e4a13c14a8282d44a564d05d6533e0be91134e8e
Vasile Cotovanu <vasile.cotovanu@gmail.com>
2024-05-30 15:00:41 +0200

    Updates model

--- apps/gtfs-rt-status/js/ts/models/response_gtfs_lookup.ts
+++ apps/gtfs-rt-status/js/ts/models/response_gtfs_lookup.ts
@@ -1,6 +1,8 @@
+import { AgencyJSON, RouteJSON, StopJSON } from '../_shared/types/gtfs/gtfs'
+
 export interface Response_GTFS_Lookup {
     lookup_name: string
     data_source: string
     rows_no: number
-    rows: GTFS.Agency[] | GTFS.Route[] | GTFS.Stop[]
+    rows: AgencyJSON[] | RouteJSON[] | StopJSON[]
 }
