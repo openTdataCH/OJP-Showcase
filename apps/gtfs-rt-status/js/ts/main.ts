59b3ecbfb3ee7dfeed82b4331e17797366a0c513
Vasile Cotovanu <vasile.cotovanu@gmail.com>
2024-05-30 14:51:24 +0200

    Construct GTFS_DB_Controller using gtfs_day supplied from catalog file which is loaded first, then called the load_resources for the resolved day

--- apps/gtfs-rt-status/js/ts/main.ts
+++ apps/gtfs-rt-status/js/ts/main.ts
@@ -2,15 +2,20 @@ import GTFS_DB_Controller from './controllers/GTFS_DB_Controller';
 import GTFS_RT_Reporter from './controllers/GTFS_RT_Reporter';
 import Progress_Controller from './controllers/Progress_Controller';
 
+const nowDate = new Date();
+
 const progress_controller = new Progress_Controller();
 progress_controller.setIdle();
 
 const gtfs_rt_reporter = new GTFS_RT_Reporter();
 
-const gtfs_db_controller = new GTFS_DB_Controller();
+const gtfs_db_controller = new GTFS_DB_Controller(nowDate);
 gtfs_db_controller.progress_controller = progress_controller;
 gtfs_db_controller.gtfs_rt_reporter = gtfs_rt_reporter;
-gtfs_db_controller.load_resources(() => {
-    console.log('loaded');
+
+gtfs_db_controller.loadGTFS_Catalog(() => {
+    gtfs_db_controller.load_resources(() => {
+        console.log('loaded');
+    });
 });
 
