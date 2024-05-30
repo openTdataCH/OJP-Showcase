97ea07f7b33f122e12227a343b29f9ab477ca73b
Vasile Cotovanu <vasile.cotovanu@gmail.com>
2024-05-30 14:31:58 +0200

    Use only gtfs_day, exit if not present

--- apps/gtfs-query/db_lookups.php
+++ apps/gtfs-query/db_lookups.php
@@ -2,12 +2,18 @@
 define('APP_PATH', dirname(__FILE__));
 include(APP_PATH . '/inc/common.php');
 
-$day = @$_GET['day'] ?: null;
-$hhmm =  @$_GET['hhmm'] ?: null;
+ini_set('memory_limit', '1024M');
+
+$gtfs_day = @$_GET['gtfs_day'] ?: null;
+
+if ($gtfs_day === null) {
+    print('error - gtfs_day is null');
+    exit(1);
+}
 
 $map_json = array();
 
-$gtfs_controller = new GTFS_DB_Controller(APP_CONFIG, $day, $hhmm);
+$gtfs_controller = new GTFS_DB_Controller(APP_CONFIG, $gtfs_day);
 $table_names = array('agency', 'routes', 'stops');
 foreach($table_names as $table_name) {
     $result_json = $gtfs_controller->query_table($table_name);
