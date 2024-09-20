81c2e2273f36b57d7890b6706ac7d58e0974f0e1
Vasile Cotovanu <vasile.cotovanu@gmail.com>
2024-09-20 22:20:03 +0200

    Fetch go, go-realtime in same script

--- tools/scripts/go-fetch-latest.py
+++ tools/scripts/go-fetch-latest.py
@@ -14,17 +14,23 @@ def main():
     script_path = Path(os.path.realpath(__file__))
     app_config = load_convenience_config(script_path)
 
-    _fetch_latest_resource(script_path, 'business-organisations')
-    resource_path = _check_latest_data_folder(app_config)
+    _run_package(script_path, app_config, 'go', 'actual_date_business_organisation')
+    _run_package(script_path, app_config, 'go-realtime', 'business_organisation_realtime')
     
-    print('STEP 3 - SYMLINK latest dataset')
-    latest_resource_path = app_config['data_paths']['go_latest']
+    print()
+    print('... DONE')
+    
+def _run_package(script_path: Path, app_config: any, package_key: str, resource_prefix: str):
+    _fetch_latest_resource(script_path, package_key)
+    resource_path = _check_latest_dataset(app_config, package_key, resource_prefix)
+    latest_resource_path = app_config['data_paths'][f'{package_key}_latest']
+    
     if os.path.islink(latest_resource_path):
         os.remove(latest_resource_path)
     os.symlink(resource_path, latest_resource_path)
     
+    print()
     print(f'=> {latest_resource_path}')
-    print('... DONE')
     
 def _fetch_latest_resource(script_path, package_key):
     # fetch latest archive
@@ -32,23 +38,24 @@ def _fetch_latest_resource(script_path, package_key):
     ckan_fetch_sh = f'{PYTHON_PATH} {ckan_fetch_cli_path} --package_key {package_key}'
     
     print('')
-    print('STEP 1 - FETCH LATEST ARCHIVE')
+    print(f'STEP {package_key}.1 - FETCH LATEST ARCHIVE')
     print(ckan_fetch_sh, flush=True)
     os.system(ckan_fetch_sh)
 
-def _check_latest_data_folder(app_config):
+def _check_latest_dataset(app_config, package_key, resource_prefix: str):
     # check latest folder
     print('')
-    print('STEP 2 - CHECK LATEST DATASET')
+    print(f'STEP {package_key}.2 - CHECK LATEST DATASET')
     
-    ckan_json_path = app_config['resource_paths']['ckan_go_json']
+    ckan_json_path = app_config['resource_paths'][f'ckan_{package_key}_json']
     ckan_json = load_json_from_file(ckan_json_path)
     ckan_data = CKAN_Data.from_ckan_json(ckan_json)
     
     ckan_resource = None
     for ckan_resource_item in ckan_data.result.resources:
+        print(ckan_resource_item.title['en'])
         resource_title: str = ckan_resource_item.title['en']
-        if not resource_title.startswith('actual_date_business_organisation_versions'):
+        if not resource_title.startswith(resource_prefix):
             continue
         
         ckan_resource = ckan_resource_item
@@ -64,12 +71,24 @@ def _check_latest_data_folder(app_config):
         print()
         print(row_delimiter_s)
         sys.exit(1)
-        
-    resources_base_folder_path = app_config['data_paths']['go_data']
-    resource_name = ckan_resource.title['en'][0:-4]
+    #
     
-    # resource name is the same as folder name
-    resource_path = Path(f'{resources_base_folder_path}/{resource_name}/{resource_name}')
+    ds_mimetype: str = ckan_resource.mimetype
+    ds_mimetype = ds_mimetype.lower().strip()
+    
+    resource_name = ckan_resource.title['en']
+    resources_base_folder_path = app_config['data_paths'][f'{package_key}_data']
+    
+    if 'zip' in ds_mimetype:
+        # the actual name is without .zip extension
+        resource_name = resource_name[0:-4]
+        # same for the folder that contains the resource
+        resource_folder_name = resource_name
+        
+        resource_path = Path(f'{resources_base_folder_path}/{resource_folder_name}/{resource_name}')
+    else:
+        resource_path = Path(f'{resources_base_folder_path}/{resource_name}')
+    # end ds_mimetype == 'zip'
     
     if not os.path.isfile(resource_path):
         print()
