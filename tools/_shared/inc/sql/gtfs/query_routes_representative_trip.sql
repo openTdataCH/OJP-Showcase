85539bf009a4686d6baa2b002eb4ab8204435d6c
Vasile Cotovanu <vasile.cotovanu@gmail.com>
2025-01-19 13:51:57 +0100

    Use also the number of stops when ranking the trips

--- tools/_shared/inc/sql/gtfs/query_routes_representative_trip.sql
+++ tools/_shared/inc/sql/gtfs/query_routes_representative_trip.sql
@@ -4,10 +4,17 @@ LEFT JOIN trips ON
 trips.trip_id IN (
     SELECT 
         trips.trip_id
-        FROM trips, calendar 
-        WHERE trips.route_id =  routes.route_id
-        AND trips.service_id = calendar.service_id
-        -- TRIPS with most day_bit '1'
-        ORDER BY LENGTH(REPLACE(calendar.day_bits, '0', '')) DESC
+        FROM trips, calendar
+        WHERE 
+            trips.route_id =  routes.route_id
+            AND trips.service_id = calendar.service_id
+        GROUP BY trips.trip_id
+        ORDER BY (
+            -- most calendar days (values: 0..360) * 10 (for normalisation)
+            LENGTH(REPLACE(calendar.day_bits, '0', '')) * 10
+            + 
+            -- max number of stops (max in ch is 80)
+			trips.stop_times_count
+        ) DESC
     LIMIT 1
 )
