371e61d9b6023d39d41f7cad02bcfc0f01047965
Vasile Cotovanu <vasile.cotovanu@gmail.com>
2025-02-01 23:58:06 +0100

    Remove the symlinks before creating them again - quickndirty for now

--- tools/scripts/cli_otd_fetch_bo.sh
+++ tools/scripts/cli_otd_fetch_bo.sh
@@ -9,6 +9,7 @@ BO_LOGFILE=$BO_BASE_LOGFILE-$DATE_NOW.log
 ./python3 $DIR/cli_otd_fetch_csv_package.py --package_id business-organisations 2>&1 | tee $BO_LOGFILE
 
 BO_LATEST_LOGFILE=$BO_BASE_LOGFILE-LATEST.log
+rm -f $BO_LATEST_LOGFILE
 ln -s $BO_LOGFILE $BO_LATEST_LOGFILE
 
 GO_BASE_LOGFILE=$LOGS_BASEPATH/otd_fetch_csv_go-realtime
@@ -16,4 +17,5 @@ GO_LOGFILE=$GO_BASE_LOGFILE-$DATE_NOW.log
 ./python3 $DIR/cli_otd_fetch_csv_package.py --package_id go-realtime 2>&1 | tee $GO_LOGFILE
 
 GO_LATEST_LOGFILE=$GO_BASE_LOGFILE-LATEST.log
+rm -f $GO_LATEST_LOGFILE
 ln -s $GO_LOGFILE $GO_LATEST_LOGFILE
