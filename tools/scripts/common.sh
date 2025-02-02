12b15ae18e6af7c986a18ced82cc642acdd2575b
Vasile Cotovanu <vasile.cotovanu@gmail.com>
2025-02-02 13:00:40 +0100

    Adds common bash helpers

---
+++ tools/scripts/common.sh
@@ -0,0 +1,17 @@
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
+_strip_filename_yyymmdd() {
+    local file="$1"
+    echo "$file" | sed -E 's/(.*)-[0-9]{4}-[0-9]{2}-[0-9]{2}.*/\1/'
+}
+
+symlink_latest() {
+    local file="$1"
+    local file_extension="${file##*.}"
+    local file_latest=$(_strip_filename_yyymmdd "$file")-LATEST."$file_extension"
+    
+    [ -L "$file_latest" ] && rm "$file_latest"
+    ln -s "$file" "$file_latest"
+}
+
+PYTHON_VENV_PATH=$DIR/../../python-venv
