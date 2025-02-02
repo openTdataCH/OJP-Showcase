e07808419c6087f8a9a840063ae1036be19bdd65
Vasile Cotovanu <vasile.cotovanu@gmail.com>
2025-02-02 13:01:18 +0100

    Use helpers for running python / symlinking

--- tools/scripts/cli_otd_fetch_bo.sh
+++ tools/scripts/cli_otd_fetch_bo.sh
@@ -4,18 +4,13 @@ LOGS_BASEPATH=$DIR/logs
 # DATE_NOW=$(date +"%Y-%m-%d-%H%M")
 DATE_NOW=$(date +"%Y-%m-%d")
 
-BO_BASE_LOGFILE=$LOGS_BASEPATH/otd_fetch_csv_business-organisations
-BO_LOGFILE=$BO_BASE_LOGFILE-$DATE_NOW.log
-$DIR/python3 $DIR/cli_otd_fetch_csv_package.py --package_id business-organisations 2>&1 | tee $BO_LOGFILE
+source $DIR/common.sh
+source $PYTHON_VENV_PATH/bin/activate
 
-BO_LATEST_LOGFILE=$BO_BASE_LOGFILE-LATEST.log
-rm -f $BO_LATEST_LOGFILE
-ln -s $BO_LOGFILE $BO_LATEST_LOGFILE
+BO_LOGFILE=$LOGS_BASEPATH/otd_fetch_csv_business-organisations-$DATE_NOW.log
+python3 $DIR/cli_otd_fetch_csv_package.py --package_id business-organisations 2>&1 | tee $BO_LOGFILE
+symlink_latest $BO_LOGFILE
 
-GO_BASE_LOGFILE=$LOGS_BASEPATH/otd_fetch_csv_go-realtime
-GO_LOGFILE=$GO_BASE_LOGFILE-$DATE_NOW.log
-$DIR/python3 $DIR/cli_otd_fetch_csv_package.py --package_id go-realtime 2>&1 | tee $GO_LOGFILE
-
-GO_LATEST_LOGFILE=$GO_BASE_LOGFILE-LATEST.log
-rm -f $GO_LATEST_LOGFILE
-ln -s $GO_LOGFILE $GO_LATEST_LOGFILE
+GO_LOGFILE=$LOGS_BASEPATH/otd_fetch_csv_go-realtime-$DATE_NOW.log
+python3 $DIR/cli_otd_fetch_csv_package.py --package_id go-realtime 2>&1 | tee $GO_LOGFILE
+symlink_latest $GO_LOGFILE
