a94204f4615b8c79a63683a8a91ca4bc13441c2e
Vasile Cotovanu <vasile.cotovanu@gmail.com>
2025-02-05 11:23:16 +0100

    Adds constants

---
+++ apps/siri-et-compare-gtfs-rt/src/app/constants.ts
@@ -0,0 +1,29 @@
+import { ReportData } from "./types/report-controller";
+
+export const AGENCY_ID_NO_DATA = 'AGENCY_ID_NO_DATA';
+export const LIST_DELIMITER = ' -•- ';
+
+export const DEFAULT_REPORT_DATA: ReportData = {
+  reportDay: 'n/a',
+  gtfsDay: 'n/a',
+
+  siriET_Total_No: 0,
+  siriET_Day_No: 0,
+  gtfsRT_Total_No: 0,
+  
+  siriET_NoAgencyItems: [],
+  gtfsRT_NoAgencyItems: [],
+
+  siriET_OnlyAgencyData: [],
+  siriET_OnlyAgencySelectedItems: [],
+  gtfsRT_OnlyAgencyData: [],
+  gtfsRT_OnlyAgencySelectedItems: [],
+
+  bothInAgency: {
+    agencyData: [],
+    siriET_NoGTFS_Items: [],
+    gtfsRT_NoGTFS_Items: [],
+    resultMatchNoGTFS_RT_Rows: [],
+    resultMatchRowsWithGTFS_RT_Rows: [],
+  },
+};
