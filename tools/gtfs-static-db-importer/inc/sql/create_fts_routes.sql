1390fd3b861f0f0677af36c3faec29e53b9afd94
Vasile Cotovanu <vasile.cotovanu@gmail.com>
2025-02-23 20:45:48 +0100

    Adds FTS routes table create / insert query

---
+++ tools/gtfs-static-db-importer/inc/sql/create_fts_routes.sql
@@ -0,0 +1,15 @@
+CREATE VIRTUAL TABLE fts_routes USING fts5(route_id, trip_stop_ids);
+
+-- populate routes with distinct stops
+INSERT INTO fts_routes (route_id, trip_stop_ids)
+SELECT
+	trips.route_id,
+	GROUP_CONCAT(DISTINCT stops_main.stop_id)
+FROM
+	trips, stop_times, stops, stops AS stops_main
+WHERE
+	trips.trip_id = stop_times.trip_id 
+	AND stop_times.stop_id = stops.stop_id	
+	AND stops.location_type != 1
+	AND stops.parent_station = stops_main.stop_id 
+GROUP BY trips.route_id;
