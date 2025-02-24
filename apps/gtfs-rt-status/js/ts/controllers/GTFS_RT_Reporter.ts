fbd73c8c0cc80565cbe8e9422047a040cdd6ed9e
Vasile Cotovanu <vasile.cotovanu@gmail.com>
2025-02-24 08:59:42 +0100

    Use new proxy for GTFS-RT

--- apps/gtfs-rt-status/js/ts/controllers/GTFS_RT_Reporter.ts
+++ apps/gtfs-rt-status/js/ts/controllers/GTFS_RT_Reporter.ts
@@ -60,7 +60,7 @@ export default class GTFS_RT_Reporter {
         this.gtfs_query_base_address = 'https://tools.odpch.ch/gtfs-rt-status/api/gtfs-query';
         
         this.report_datetime = new Date();
-        this.gtfs_rt_url = 'https://www.webgis.ro/tmp/proxy-gtfsrt2020/gtfsrt2020';
+        this.gtfs_rt_url = 'https://tools.odpch.ch/data/gtfs-rt/gtfs-rt-latest.json';
 
         if (customReportFilename !== null) {
             const reportDateTimeMatches = customReportFilename.match(/([0-9]{4})-([0-9]{2})-([0-9]{2})-([0-9]{2})([0-9]{2})/);
