e0f4f47036c5bb87e9611af8eac3b7ab054ef427
Vasile Cotovanu <vasile.cotovanu@gmail.com>
2025-02-25 23:43:23 +0100

    Check if the route can be matched from Atlas OEV

--- apps/atlas-routes-compare-gtfs/src/app/controllers/match-controller.ts
+++ apps/atlas-routes-compare-gtfs/src/app/controllers/match-controller.ts
@@ -8,7 +8,7 @@ import { FormatHelpers } from "../helpers/format-helpers"
 
 import { AtlasLineDataController, AtlasRouteCSVRow, AtlasStopGeoJSONFeature } from "../../shared/controllers/atlas-data";
 import { BusinessOrganisationsController } from "../../shared/controllers/business-organisations";
-import { AgencyReportRow, AgencyRouteReportRow, GTFS_RouteReportRow, ReportData } from "../types/_all";
+import { AgencyReportRow, AgencyRouteReportRow, AtlasOEV_RouteReportRow, GTFS_RouteReportRow, ReportData } from "../types/_all";
 
 import { DEBUG_ROUTE_IDs } from "../constants";
 
@@ -30,18 +30,22 @@ export class MatchController {
   private atlasLinieController: AtlasLineDataController; 
   private mapRouteTrips: Record<string, Trip>; 
   private mapAtlasRouteFeatures: Record<string, AtlasStopGeoJSONFeature[]>;
+  private mapAtlasOEV_Routes: Record<string, AtlasOEV_RouteReportRow>;
 
   constructor(
     gtfsDBController: GTFS_DB_Controller, 
     boController: BusinessOrganisationsController, 
     atlasLinieController: AtlasLineDataController, 
     mapRouteTrips: Record<string, Trip>, 
-    mapAtlasRouteFeatures: Record<string, AtlasStopGeoJSONFeature[]>) {
+    mapAtlasRouteFeatures: Record<string, AtlasStopGeoJSONFeature[]>,
+    mapAtlasOEV_Routes: Record<string, AtlasOEV_RouteReportRow>,
+  ) {
       this.gtfsDBController = gtfsDBController;
       this.boController = boController;
       this.atlasLinieController = atlasLinieController;
       this.mapRouteTrips = mapRouteTrips;
       this.mapAtlasRouteFeatures = mapAtlasRouteFeatures;
+      this.mapAtlasOEV_Routes = mapAtlasOEV_Routes;
   }
 
   public process(reportData: ReportData) {
@@ -160,7 +164,41 @@ export class MatchController {
     reportData.agencyFilterReportRows = Array.from(reportData.agencyReportRows);
   }
 
+  private matchRouteByAgencyAndNumber(routeReportRow: AgencyRouteReportRow, matchIndexes: MatchIndexes, agencyRoutes: Route[]) {
+    const swissLineNumber = routeReportRow.atlasRoute.swissLineNumber;
+    const swissLineNumberParts = swissLineNumber.split('.');
+    const swissLineNumberCat = swissLineNumberParts[0];
 
+    if (swissLineNumberCat === 'c') {
+      // AV Autoverlad is not in GTFS
+      routeReportRow.matchedStatus = 'NO_MATCHES';
+      return;
+    }
+
+    if (swissLineNumber.endsWith(':K')) {
+      // meta routes, i.e. multiple routes
+      routeReportRow.matchedStatus = 'NO_MATCHES';
+      return;
+    }
+
+    // TRY to see if PDF has already a GTFS route
+    const slnid = routeReportRow.atlasRoute.slnid;
+    const atlasPDF_Route = this.mapAtlasOEV_Routes[slnid] ?? null;
+    if (atlasPDF_Route && atlasPDF_Route.status === 'OK_TRIP') {
+      const gtfsRouteId = atlasPDF_Route.gtfs_route_id ?? null;
+      if (gtfsRouteId) {
+        const gtfsRouteRoute = this.gtfsDBController.mapRoutes[gtfsRouteId] ?? null;
+        if (gtfsRouteRoute) {
+          routeReportRow.matchedStatus = 'OK_EXT';
+          routeReportRow.matchedGTFS_Route = gtfsRouteRoute;
+
+          const oevId = swissLineNumberParts.slice(1).join('.');
+          const oevYear = '2025'; // TODO - add this in config?
+          routeReportRow.oevLink = 'https://www.oev-info.ch/de/fahrplan-aktuell/fahrplanfelder/' + oevYear + '-' + oevId;
+          return;
+        }
+      }
+    }
 
     // TRY special case - 1 GTFS route - 1 AtlasRoute for same agency
     const atlasRoutesNo = this.atlasLinieController.mapAgencyRows[routeReportRow.atlasRoute.businessOrganisation].length;
@@ -171,6 +209,7 @@ export class MatchController {
     }
 
     // TRY 1-1 match for agency_id, route_short_name
+    const routeNumber = routeReportRow.atlasRoute.number;
     const agencyRouteNumberRoutes = agencyRoutes.filter(el => el.route_short_name === routeNumber);
     if (agencyRouteNumberRoutes.length === 1) {
       routeReportRow.matchedStatus = 'OK';
