00814c6a6a807dde69c646869ece48ea843ceaee
Vasile Cotovanu <vasile.cotovanu@gmail.com>
2025-02-25 21:05:01 +0100

    Adds lookup OEV Python call

--- tools/scripts/cli_otd_fetch_atlas.sh
+++ tools/scripts/cli_otd_fetch_atlas.sh
@@ -11,6 +11,10 @@ ATLAS_FETCH_LOGFILE=$LOGS_BASEPATH/otd_fetch_csv_atlas-lines-$DATE_NOW.log
 python3 $DIR/cli_otd_fetch_csv_package.py --package_id slnid-line 2>&1 | tee $ATLAS_FETCH_LOGFILE
 symlink_latest $ATLAS_FETCH_LOGFILE
 
+ATLAS_LOOKUP_OEV_LOGFILE=$LOGS_BASEPATH/otd_lookup_oev_atlas-lines-$DATE_NOW.log
+python3 $DIR/../atlas-lookup-lines/cli_run_lookup_lines.py 2>&1 | tee $ATLAS_LOOKUP_OEV_LOGFILE
+symlink_latest $ATLAS_LOOKUP_OEV_LOGFILE
+
 ATLAS_GEOCODE_LOGFILE=$LOGS_BASEPATH/otd_geocode_atlas-lines-$DATE_NOW.log
 npm --prefix $DIR/../atlas-geocode-lines/ run process_node18 2>&1 | tee $ATLAS_GEOCODE_LOGFILE
 symlink_latest $ATLAS_GEOCODE_LOGFILE
