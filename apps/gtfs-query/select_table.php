45ee6d7b5782002274b47a9679948f7a77de7fd4
Vasile Cotovanu <vasile.cotovanu@gmail.com>
2025-03-02 11:08:18 +0100

    Use correct params

--- apps/gtfs-query/select_table.php
+++ apps/gtfs-query/select_table.php
@@ -2,14 +2,13 @@
 define('APP_PATH', dirname(__FILE__));
 include(APP_PATH . '/inc/common.php');
 
-$day = @$_GET['day'] ?: null;
-$hhmm =  @$_GET['hhmm'] ?: null;
 $table_name = @$_GET['table_name'] ?: null;
+$gtfs_day = @$_GET['gtfs_day'] ?: 'LATEST';
 
 if (!$table_name) {
     die('No lookup table given');
 }
 
-$gtfs_controller = new GTFS_DB_Controller(APP_CONFIG, $day, $hhmm);
+$gtfs_controller = new GTFS_DB_Controller(APP_CONFIG, $gtfs_day);
 $result_json = $gtfs_controller->query_table($table_name);
 JsonView::dump($result_json);
