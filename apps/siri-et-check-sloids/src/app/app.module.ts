9ec5c07e5fd4e75323a1a38b9e7792ebd33f2e67
Vasile Cotovanu <vasile.cotovanu@gmail.com>
2025-03-06 11:02:30 +0100

    Adds forms / http service dependencies

--- apps/siri-et-check-sloids/src/app/app.module.ts
+++ apps/siri-et-check-sloids/src/app/app.module.ts
@@ -1,5 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -10,7 +12,10 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule,
+    ReactiveFormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
