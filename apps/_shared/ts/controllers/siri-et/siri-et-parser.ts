f265ec842cb81533f8ba08c4e2f07ca60f1afb66
Vasile Cotovanu <vasile.cotovanu@gmail.com>
2025-03-17 15:38:11 +0100

    Reuse SIRI_ET_Parser

---
+++ apps/_shared/ts/controllers/siri-et/siri-et-parser.ts
@@ -0,0 +1,65 @@
+import { BaseXMLParser } from '../xml-parser/base-xml-parser';
+import { VehicleJourney } from '../../models/siri-et/vehicle-journey';
+
+interface XML_Parser_Response {
+  status: 'ERROR' | 'COUNT_ITEMS_NO' | 'PARSE.ITEM' | 'PARSE.DONE',
+  itemsNo: number,
+  items: any[],
+};
+
+export type XML_Parser_Callback = (response: XML_Parser_Response) => void;
+
+export class SIRI_ET_Parser extends BaseXMLParser {
+  private itemsNo: number; 
+  private vehicleJourneys: VehicleJourney[];
+  public callback: XML_Parser_Callback | null;
+
+  constructor() {
+    super();
+
+    this.itemsNo = 0;
+    this.vehicleJourneys = [];
+    this.callback = null;
+  }
+
+  public override parseXML(responseXMLText: string): void {
+    this.vehicleJourneys = [];
+
+    if (this.callback) {
+      this.itemsNo = responseXMLText.split('</EstimatedVehicleJourney>').length - 1;
+      this.callback({
+        status: 'COUNT_ITEMS_NO',
+        itemsNo: this.itemsNo,
+        items: this.vehicleJourneys,
+      });
+    }
+
+    super.parseXML(responseXMLText);
+  }
+
+  protected override onCloseTag(nodeName: string): void {
+    if (nodeName === 'EstimatedVehicleJourney') {
+      const vehicleJourney = VehicleJourney.initWithTreeNode(this.currentNode);
+      if (vehicleJourney) {
+        this.vehicleJourneys.push(vehicleJourney);
+        if (this.callback) {
+          this.callback({
+            status: 'PARSE.ITEM',
+            itemsNo: this.itemsNo,
+            items: this.vehicleJourneys,
+          });
+        }
+      }
+    }
+  }
+
+  protected override onEnd(): void {
+    if (this.callback) {
+      this.callback({
+        status: 'PARSE.DONE',
+        itemsNo: this.itemsNo,
+        items: this.vehicleJourneys,
+      });
+    }
+  }
+}
