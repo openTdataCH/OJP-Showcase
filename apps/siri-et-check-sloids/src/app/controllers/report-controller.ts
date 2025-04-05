b439213787263a58a7d558dcad2ef5ae99c178d3
Vasile Cotovanu <vasile.cotovanu@gmail.com>
2025-04-05 16:33:51 +0200

    Use new modelUse different model for processing data (report Controller)

--- apps/siri-et-check-sloids/src/app/controllers/report-controller.ts
+++ apps/siri-et-check-sloids/src/app/controllers/report-controller.ts
@@ -1,5 +1,16 @@
+import { VehicleJourney } from "../../shared/models/siri-et/vehicle-journey";
 import { BusinessOrganisationsController } from "../../shared/controllers/business-organisations";
-import { AgencyReportRow, MapAgencySIRI_ET_Journeys, ReportData, ServiceCallStop, VehicleJourneyReportRow } from "../types/_all";
+import { MapAgencySIRI_ET_Journeys } from "../../shared/helpers/siri-et-helpers";
+
+import { ReportData, StopReportOrganisationRow, StopReportRow } from "../types/_all";
+
+type StopWithIssues = {
+  didokRef: string,
+  name: string,
+  mapSloids: Record<string, boolean>,
+  // AgencyId > Line > VehicleJourney[]
+  mapAgencyLinesMessages: Record<string, Record<string, VehicleJourney[]>>,
+};
 
 export class ReportController {
   private mapAgencySIRI_ET_Journeys: MapAgencySIRI_ET_Journeys;
@@ -12,101 +23,138 @@ export class ReportController {
 
   public processData(reportData: ReportData) {
     reportData.siriET_totalIssuesNo = 0;
-    reportData.agencyReportRows = [];
+    reportData.stopReportRows = [];
+
+    const mapStopsSloidIssues: Record<string, StopWithIssues> = {};
 
     for (const agencyId in this.mapAgencySIRI_ET_Journeys) {
       const vehicleJourneys = this.mapAgencySIRI_ET_Journeys[agencyId];
 
-      const boRow = this.boController.mapOrganisationNumber[agencyId] ?? null;
-      const agencyTitle: string = (() => {
-        if (boRow === null) {
-          return 'NO_AGENCY_DATA';
-        }
-
-        const title = boRow.descriptionDe + ' (' + boRow.abbreviationDe + ')';
-        return title;
-      })();
-      
-      const agencyReportRow: AgencyReportRow = {
-        agencyTitle: agencyTitle,
-        agencyURL: null,
-        totalMessagesNo: vehicleJourneys.length,
-        
-        organisation: boRow,
-        stopsWithIssues: [],
-        vehicleJourneyReportRows: [],
-      };
-
-      if (boRow) {
-        agencyReportRow.agencyURL = 'https://atlas.app.sbb.ch/business-organisation-directory/business-organisations/' + boRow.sboid;
-      }
-
-      const mapAgencyStopWithIssues: Record<string, ServiceCallStop> = {};
-      
       vehicleJourneys.forEach(vehicleJourney => {
-        const stops: ServiceCallStop[] = [];
-        vehicleJourney.serviceCalls.forEach(serviceCall => {
-          const hasIssues = (() => {
-            if (serviceCall.stopPointRef.startsWith('85')) {
-              return true;
-            }
-            if (serviceCall.stopPointRef.startsWith('ch:1:ScheduledStopPoint:85')) {
-              return true;
-            }
+        const serviceLine = vehicleJourney.publishedLineName;
 
+        vehicleJourney.serviceCalls.forEach(serviceCall => {
+          const stopPointRef = serviceCall.stopPointRef;
+
+          const hasIssues = (() => {
+            if (stopPointRef.startsWith('85')) {
+              return true;
+            }
+            if (stopPointRef.startsWith('ch:1:ScheduledStopPoint:85')) {
+              return true;
+            }
+  
             return false;
           })();
 
-          const serviceCallStop: ServiceCallStop = {
-            hasSloidIssue: hasIssues,
-            stopPointRef: serviceCall.stopPointRef,
-            stopPointName: serviceCall.stopPointName ?? 'n/a stopPointName',
-            
-            // update this only for `hasIssues`
-            didokRef: 'n/a',
-            url: 'n/a',
-          };
-          
-          if (hasIssues) {
-            serviceCallStop.didokRef = serviceCall.stopPointRef.replace('ch:1:ScheduledStopPoint:', '');
-            if (serviceCallStop.didokRef.length !== 7) {
-              // strip last digits for ScheduledStopPoint
-              serviceCallStop.didokRef = serviceCallStop.didokRef.slice(0, 7);
-            }
-            
-            serviceCallStop.url = 'https://atlas.app.sbb.ch/service-point-directory/service-points/' + serviceCallStop.didokRef + '/service-point';
-
-            reportData.siriET_totalIssuesNo += 1;
-
-            if (!(serviceCallStop.didokRef in mapAgencyStopWithIssues)) {
-              mapAgencyStopWithIssues[serviceCallStop.didokRef] = serviceCallStop;
-            }
+          if (!hasIssues) {
+            return;
           }
 
-          stops.push(serviceCallStop);
+          reportData.siriET_totalIssuesNo += 1;
+
+          const didokRef = (() => {
+            let ref = stopPointRef.replace('ch:1:ScheduledStopPoint:', '');
+            if (ref.length !== 7) {
+              ref = ref.slice(0, 7);
+            }
+
+            return ref;
+          })();
+
+          if (!(didokRef in mapStopsSloidIssues)) {
+            const stopName = serviceCall.stopPointName ?? 'n/a stopPointName';
+            const stopsSloidIssues: StopWithIssues = {
+              didokRef: didokRef,
+              name: stopName,
+              mapSloids: {},
+              mapAgencyLinesMessages: {},
+            };
+
+            mapStopsSloidIssues[didokRef] = stopsSloidIssues;
+          }
+
+          mapStopsSloidIssues[didokRef].mapSloids[stopPointRef] = true;
+
+          if (!(agencyId in mapStopsSloidIssues[didokRef].mapAgencyLinesMessages)) {
+            mapStopsSloidIssues[didokRef].mapAgencyLinesMessages[agencyId] = {};
+          }
+
+          if (!(serviceLine in mapStopsSloidIssues[didokRef].mapAgencyLinesMessages[agencyId])) {
+            mapStopsSloidIssues[didokRef].mapAgencyLinesMessages[agencyId][serviceLine] = [];
+          }
+          mapStopsSloidIssues[didokRef].mapAgencyLinesMessages[agencyId][serviceLine].push(vehicleJourney);
         });
-
-        const stopsWithIssues = stops.filter(el => el.hasSloidIssue);
-
-        if (stopsWithIssues.length > 0) {
-          const reportRow: VehicleJourneyReportRow = {
-            vehicleJourney: vehicleJourney,
-            stopsWithIssues: stopsWithIssues,
-            stops: stops,
-          };
-          
-          agencyReportRow.vehicleJourneyReportRows.push(reportRow);
-        }
       });
-
-      if (agencyReportRow.vehicleJourneyReportRows.length > 0) {
-        agencyReportRow.stopsWithIssues = Object.values(mapAgencyStopWithIssues);
-        reportData.agencyReportRows.push(agencyReportRow);
-      }
     }
 
-    reportData.agencyReportRows = reportData.agencyReportRows.sort((a, b) => b.vehicleJourneyReportRows.length - a.vehicleJourneyReportRows.length);
+    for (const didokRef in mapStopsSloidIssues) {
+      const url = 'https://atlas.app.sbb.ch/service-point-directory/service-points/' + didokRef + '/service-point';
+      const stopsSloidIssues = mapStopsSloidIssues[didokRef];
 
-    console.log(reportData.agencyReportRows);
+      const stopReportRow: StopReportRow = {
+        didokRef: didokRef,
+        stopPointName: stopsSloidIssues.name,
+        url: url,
+        sloidIssues: Object.keys(stopsSloidIssues.mapSloids),
+
+        organisations: [],
+        totalAffectedMessagesNo: 0,
+
+        debugMap1: stopsSloidIssues,
+      };
+
+      for (const agencyId in stopsSloidIssues.mapAgencyLinesMessages) {
+        const publishedLineNumbers = Object.keys(stopsSloidIssues.mapAgencyLinesMessages[agencyId]);
+
+        const boData = this.boController.mapOrganisationNumber[agencyId] ?? null;
+        const agencyTitle: string = (() => {
+          if (boData === null) {
+            return 'NO_AGENCY_DATA';
+          }
+
+          const title = boData.descriptionDe + ' (' + boData.abbreviationDe + ')';
+          return title;
+        })();
+
+        const agencyURL: string = (() => {
+          let baseURL = 'https://atlas.app.sbb.ch/business-organisation-directory/business-organisations';
+
+          if (boData === null) {
+            return baseURL;
+          }
+
+          const url = baseURL + '/' + boData.sboid;
+          return url;
+        })();
+
+        const sboid = boData.sboid ?? 'n/a SBOID for AgencyId: ' + agencyId;
+
+        const stopReportOrganisationRow: StopReportOrganisationRow = {
+          sboid: sboid,
+          url: agencyURL,
+          agencyTitle: agencyTitle,
+          publishedLineNumbers: publishedLineNumbers,
+        };
+
+        for (const lineNumber in stopsSloidIssues.mapAgencyLinesMessages[agencyId]) {
+          const itemsNo = stopsSloidIssues.mapAgencyLinesMessages[agencyId][lineNumber].length;
+          stopReportRow.totalAffectedMessagesNo += itemsNo;
+        }
+
+        stopReportRow.organisations.push(stopReportOrganisationRow);
+      }
+
+      reportData.stopReportRows.push(stopReportRow);
+    }
+
+    reportData.stopReportRows = reportData.stopReportRows.sort((a, b) => {
+      const scoreA = a.organisations.length * 10 + a.totalAffectedMessagesNo / 1000;
+      const scoreB = b.organisations.length * 10 + b.totalAffectedMessagesNo / 1000;
+
+      return scoreB - scoreA;
+    });
+
+    console.log(reportData.stopReportRows);
   }
 }
