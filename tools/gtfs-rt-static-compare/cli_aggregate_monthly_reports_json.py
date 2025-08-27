ed47b0338f0e1ca86f089372fb1ffb3280a6d9e1
Vasile Cotovanu <vasile.cotovanu@gmail.com>
2025-08-27 15:55:48 +0200

    Use new approach for computing monthly reports - can be run also for the all months

--- tools/gtfs-rt-static-compare/cli_aggregate_monthly_reports_json.py
+++ tools/gtfs-rt-static-compare/cli_aggregate_monthly_reports_json.py
@@ -2,84 +2,254 @@ import os, sys
 
 import re
 import argparse
-from datetime import datetime
+from datetime import datetime, date, timedelta
 
 from pathlib import Path
 
-from typing import List, Dict
+from typing import Any, List, Optional
+
+from statistics import mean, median
 
 from compare_gtfs_rt_static.helpers.config_helpers import load_yaml_config
 from compare_gtfs_rt_static.helpers.json_helpers import load_json_from_file, export_json_to_file
 from compare_gtfs_rt_static.helpers.log_helpers import log_message
 
-from compare_gtfs_rt_static.models.gtfs_rt_static_report import GTFS_RT_Static_Report, GTFS_RT_Static_Monthly_Report
+from compare_gtfs_rt_static.models.gtfs_rt_static_report import GTFS_RT_Static_Report, GTFS_RT_Static_Monthly_Report, GTFS_RT_Static_Report_Compare_Info, GTFS_RT_Static_Report_Metadata
+from compare_gtfs_rt_static.gtfs_controller import gtfs_rt_static_report_file_regexp
 
-def main():
-    app_path = Path(os.path.realpath(__file__)).parent
-    config_path = f'{app_path}/config/config.yml'
+is_verbose_mode = False
+
+def _compute_compare(app_config: Any, report_key: str, map_hr_report: dict[str, GTFS_RT_Static_Report_Metadata], map_compare: dict[str, GTFS_RT_Static_Report_Compare_Info]):
+    compare_info = GTFS_RT_Static_Report_Compare_Info('h', {})
     
-    app_config = load_yaml_config(config_path, app_path)
-    report_template_path: str = app_config['resource_paths']['gtfs_rt_static_report']
-    report_root_path = Path(report_template_path.split('[YEAR]/[MONTH]/[DAY]')[0])
+    report_ymd_f = report_key[0:10]
+    report_hr_f = report_key[-2:]
     
-    report_now = datetime.now()
-    report_ym = report_now.strftime('%Y-%m')
+    if report_ymd_f in app_config['map_holidays']:
+        return compare_info
     
-    parser = argparse.ArgumentParser()
-    parser.add_argument('--month', '--month', default=report_ym)
-    args = parser.parse_args()
+    report_ymd = date.fromisoformat(report_ymd_f)
     
-    report_ym = args.month
+    # Monday == 0 ... Sunday == 6
+    report_weekday = report_ymd.weekday()
+    report_hr = int(report_hr_f)
+    # GTFS day starts at 4:00, everything earlier belongs to the previous day
+    if report_hr < 4:
+        report_weekday -= 1
+        if report_weekday == -1:
+            report_weekday = 6
+            
+    if report_weekday > 4:
+        return compare_info
     
-    log_message(f'START aggregate reports for {report_ym}')
+    compare_info.compare_type = 'w'
     
-    report_month_parts = report_ym.split('-')
-    report_year = report_month_parts[0]
-    report_month = report_month_parts[1]
-    
-    report_month_part = Path(f'{report_root_path}/{report_year}/{report_month}')
-    report_file_paths: List[Path] = []
-    for gtfs_rt_report_path in report_month_part.rglob('*.json'):
-        report_file_paths.append(gtfs_rt_report_path)
-    report_file_paths.sort()
-    
-    map_report_days: Dict[str, Dict[str, GTFS_RT_Static_Report]] = {}
-    for gtfs_rt_report_path in report_file_paths:
-        gtfs_rt_report_json = load_json_from_file(gtfs_rt_report_path)
-        gtfs_rt_report = GTFS_RT_Static_Report.from_json(gtfs_rt_report_json)
+    prev_report_day = date.fromisoformat(report_ymd_f)
+    days_back_idx = 0
+    days_back_max = 100
+    days_compare_max = 10
+    while days_back_idx < days_back_max:
+        prev_report_day -= timedelta(days=1)
         
-        file_matches = re.match(r"gtfs_rt_static_report-([0-9]{4})-([0-9]{2})-([0-9]{2})-([0-9]{2})([0-9]{2})", gtfs_rt_report_path.name)
+        prev_report_key = f'{prev_report_day}-{report_hr_f}'
+        if prev_report_key in map_hr_report:
+            prev_compare = map_compare.get(prev_report_key, None)
+        
+            is_prev_working_day = False
+            if prev_compare is not None:
+                if prev_compare.compare_type == 'w':
+                    is_prev_working_day = True
+            # check if prev compare is working day
+            
+            if is_prev_working_day:
+                prev_total_rows_no = map_hr_report[prev_report_key].total_rows_no
+                compare_info.map_days[f'{prev_report_day}'] = prev_total_rows_no
+            # if
+        # check if we have report
+        
+        if len(compare_info.map_days.keys()) >= days_compare_max:
+            break
+            
+        days_back_idx += 1
+    # while days back
+    
+    is_relevant = (6 <= report_hr <= 20)
+    has_prev_data = len(compare_info.map_days.keys()) == days_compare_max
+    if has_prev_data and is_relevant:
+        prev_values = compare_info.map_days.values()
+        measure_mean = median(prev_values)
+        drop_line = measure_mean * (1 - 0.1)
+        
+        report = map_hr_report[report_key]
+        report_total_rows_no = report.total_rows_no
+        
+        if report_total_rows_no < drop_line:
+            compare_info.compare_type = 'w_p'
+            
+            if is_verbose_mode:
+                print(f'possible issue:')
+                print(f'  {report_ymd_f} {report_hr_f}:00')
+                print(f'  rows      : {report_total_rows_no}')
+                print(f'  drop_line : {drop_line}')
+                print()
+                
+                for prev_day, prev_day_no in  compare_info.map_days.items():
+                    print(f'    {prev_day}: {prev_day_no}')
+                    
+                print()
+            # verbose
+        # if drop line was reached
+            
+    # check if we should analyse
+    
+    return compare_info
+
+def _fetch_map_reports(app_config: Any, report_filter_ym: str) -> dict[str, GTFS_RT_Static_Report_Metadata]:
+    report_template_path: str = app_config['resource_paths']['gtfs_rt_static_report']
+    report_base_path = Path(report_template_path.split('[YEAR]/[MONTH]/[DAY]')[0])
+    
+    report_filter_months: Optional[List[str]] = None
+    if report_filter_ym != 'ALL':
+        report_filter_ym_parts = report_filter_ym.split('-')
+        dir_y_s = report_filter_ym_parts[0]
+        dir_m_s = report_filter_ym_parts[1]
+        dir_ym = f'{dir_y_s}-{dir_m_s}'
+        
+        prev_dir_y = int(dir_y_s)
+        prev_dir_m = int(dir_m_s) - 1
+        if prev_dir_m == 0:
+            prev_dir_y -= 1
+            prev_dir_m = 12
+        prev_dir_y_s = f'{prev_dir_y}'.zfill(2)
+        prev_dir_m_s = f'{prev_dir_m}'.zfill(2)
+        prev_dir_ym = f'{prev_dir_y_s}-{prev_dir_m_s}'
+        
+        report_filter_months = [prev_dir_ym, dir_ym]
+    # end build yyyy-mm filter
+    
+    report_subdirs: List[Path] = []
+    for report_subdir in report_base_path.glob('*/*'):
+        if not report_subdir.is_dir():
+            continue
+        
+        if report_filter_months is not None:
+            report_subdir_paths = f'{report_subdir}'.split('/')
+            report_subdir_ym = f'{report_subdir_paths[-2]}-{report_subdir_paths[-1]}'
+            if report_subdir_ym not in report_filter_months:
+                continue
+        # check if filter YYYY-MM is active and matches subdir
+        
+        report_subdirs.append(report_subdir)
+    # loop subdirs
+    report_subdirs.sort()
+    
+    report_file_paths: List[Path] = []
+    for report_subdir in report_subdirs:
+        report_subdir_paths = f'{report_subdir}'.split('/')
+        report_year_f = report_subdir_paths[-2]
+        report_month_f = report_subdir_paths[-1]
+        report_month_path = Path(f'{report_base_path}/{report_year_f}/{report_month_f}')
+        for gtfs_rt_report_path in report_month_path.rglob('*.json'):
+            report_file_paths.append(gtfs_rt_report_path)
+        # loop subdir JSON files
+    # loop subdirs
+    report_file_paths.sort()
+    log_message(f'... found {len(report_file_paths)} files')
+    
+    map_reports: dict[str, GTFS_RT_Static_Report_Metadata] = {}
+    for report_path in report_file_paths:
+        file_matches = re.match(gtfs_rt_static_report_file_regexp, report_path.name)
         if file_matches is None:
             print('ERROR - unknown file pattern')
-            print(gtfs_rt_report_path)
+            print(report_path)
             sys.exit(1)
-            
-        report_day = file_matches[3]
-        report_hhmm = file_matches[4] + file_matches[5]
         
-        if report_day not in map_report_days:
-            map_report_days[report_day] = {}
-            
-        map_report_days[report_day][report_hhmm] = gtfs_rt_report.metadata.as_json()
-    # report_file_paths
+        report_json = load_json_from_file(report_path)
+        report = GTFS_RT_Static_Report.from_json(report_json)
+        
+        report_year_f = file_matches[1]
+        report_month_f = file_matches[2]
+        report_day_f = file_matches[3]
+        report_hour_f = file_matches[4]
+        
+        report_key = f'{report_year_f}-{report_month_f}-{report_day_f}-{report_hour_f}'
+        
+        map_reports[report_key] = report.metadata
+    # loop report_file_paths
     
+    return map_reports
+
+def _compute_and_save_monthly_report(app_config: Any, report_filter_ym: str, map_reports: dict[str, GTFS_RT_Static_Report_Metadata], map_compare: dict[str, GTFS_RT_Static_Report_Compare_Info]): 
+    #                   YYYY-MM  >    DD    >   HH > GTFS_RT_Static_Report_Metadata
+    map_reports_by_hr: dict[str, dict[str, dict[str, GTFS_RT_Static_Report_Metadata]]] = {}
+    for report_key, report in map_reports.items():
+        # 2025-07-01-00
+        report_key_parts = report_key.split('-')
+        report_ym = '-'.join(report_key_parts[0:2])
+        report_day_f = report_key_parts[2]
+        report_hour_f = f'{report_key_parts[3]}00'
+        
+        if report_filter_ym != 'ALL':
+            if report_ym != report_filter_ym:
+                continue
+        
+        if report_ym not in map_reports_by_hr:
+            map_reports_by_hr[report_ym] = {}
+        
+        if report_day_f not in map_reports_by_hr[report_ym]:
+            map_reports_by_hr[report_ym][report_day_f] = {}
+            
+        map_reports_by_hr[report_ym][report_day_f][report_hour_f] = report
+    # loop reports
+    
+    report_now = datetime.now()
     report_now_f = report_now.strftime('%Y-%m-%d %H:%M:%S')
     report_comments = f'generated {report_now_f} with {Path(__file__).name}'
     
-    gtfs_rt_static_monthly_report = GTFS_RT_Static_Monthly_Report(
-        last_update_dt=report_now_f,
-        comments=report_comments,
-        report_days=map_report_days
-    )
+    for report_ym, report_data in map_reports_by_hr.items():
+        report_year_f, _, report_month_f = report_ym.partition('-')
+        
+        monthly_report = GTFS_RT_Static_Monthly_Report(
+            last_update_dt=report_now_f,
+            comments=report_comments,
+            report_days=report_data,
+        )
+        monthly_report_json = monthly_report.as_json()
+        
+        monthly_report_path_s: str = app_config['resource_paths']['gtfs_rt_static_mo_json']
+        monthly_report_path_s = monthly_report_path_s.replace('[YEAR]', report_year_f)
+        monthly_report_path_s = monthly_report_path_s.replace('[MONTH]', report_month_f)
+        monthly_report_path = Path(monthly_report_path_s)
+        
+        export_json_to_file(monthly_report_json, json_path=monthly_report_path, pretty_print=True)
+        log_message(f'... saved {report_ym} to {monthly_report_path.name}')
+    # loop months
+
+def main():
+    app_path = Path(os.path.realpath(__file__)).parent
+    config_path = Path(f'{app_path}/config/config.yml')
+    app_config = load_yaml_config(config_path, app_path)
     
-    report_path: str = app_config['resource_paths']['gtfs_rt_static_mo_json']
-    report_path = report_path.replace('[YEAR]', report_year)
-    report_path = report_path.replace('[MONTH]', report_month)
+    report_now = datetime.now()
     
-    export_json_to_file(gtfs_rt_static_monthly_report.as_json(), json_path=report_path, pretty_print=True)
-    log_message(f'... saved {len(report_file_paths)} reports to {report_path}')
+    default_filter_ym = report_now.strftime('%Y-%m')
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--month', '--month', default=default_filter_ym)
+    args = parser.parse_args()
+    report_filter_ym = args.month
     
-    log_message(f'... DONE')
+    log_message(f'START cli_aggregate_monthly_reports_json with --month={report_filter_ym}')
+    print()
+    
+    map_reports = _fetch_map_reports(app_config, report_filter_ym)
+    log_message(f'... done parsing {len(map_reports.keys())} reports')
+    print()
+    
+    
+    _compute_and_save_monthly_report(app_config, report_filter_ym, map_reports, map_compare)
+    print()
+    
+    log_message('... DONE')
 
 if __name__ == "__main__":
     main()
