0b230355a7c6ffea8124bc47ea41e04a286e65e6
Vasile Cotovanu <vasile.cotovanu@gmail.com>
2025-09-05 13:50:48 +0200

    source .env and .env.local if present

--- tools/scripts/common.sh
+++ tools/scripts/common.sh
@@ -1,5 +1,12 @@
 COMMON_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
+set -a
+
+source "$COMMON_DIR/.env"
+if [ -f "$COMMON_DIR/.env.local" ]; then
+  source "$COMMON_DIR/.env.local"
+fi
+
 _strip_filename_yyymmdd() {
     local file="$1"
     echo "$file" | sed -E 's/(.*)-[0-9]{4}-[0-9]{2}-[0-9]{2}.*/\1/'
