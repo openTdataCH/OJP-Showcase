763f100144c58ec187d53d96fdab8be8792241cb
Vasile Cotovanu <vasile.cotovanu@gmail.com>
2025-09-07 11:09:47 +0200

    Wording

--- apps/gtfs-rt-static-report/src/app/app.component.ts
+++ apps/gtfs-rt-static-report/src/app/app.component.ts
@@ -86,7 +86,7 @@ interface PageModel {
 const mapReportValueLookups: Record<ReportValueLookupType, string> = {
   gtfs_db_age: 'GTFS-DB Age',
   gtfs_rt_age: 'GTFS-RT Age',
-  total_rows_no: 'GTFS-RT Trips',
+  total_rows_no: 'GTFS-RT Total Trips No',
   tripOK_routeOK_no: 'Matched Trips',
   tripOK_routeNOK_no: 'Matched Trips / Not-matched Routes',
   tripNOK_routeOK_no: 'Not-matched Trips / Matched Routes',
