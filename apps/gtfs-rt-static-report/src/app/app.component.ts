0c48de5a6b8e8fd7a05c3a777be86b66d614f5dc
Vasile Cotovanu <vasile.cotovanu@gmail.com>
2025-09-07 11:09:39 +0200

    Adds compare info models

--- apps/gtfs-rt-static-report/src/app/app.component.ts
+++ apps/gtfs-rt-static-report/src/app/app.component.ts
@@ -36,13 +36,29 @@ interface GTFS_RT_Static_Report_Metadata_JSON {
   tripNOK_NOJP_no: number
 }
 
+interface GTFS_RT_Static_Report_Compare_JSON {
+  compare_type: 'h' | 'w' | 'w_p'
+  map_days: Record<string, number>
+  mean_value: Number
+  drop_line: Number
+}
+
 export interface GTFS_RT_Static_Monthly_Report_JSON {
   last_update_dt: string
   comments: string
   report_days: Record<string, Record<string, GTFS_RT_Static_Report_Metadata_JSON>>
+  compare_days: Record<string, Record<string, GTFS_RT_Static_Report_Compare_JSON>>
 }
 
 type CellClassDB = 'odd' | 'even';
+
+interface GTFS_RT_StaticReportCompareMetadata {
+  info: GTFS_RT_Static_Report_Compare_JSON
+  reportLines: string[]
+  valueF: string
+  meanValueF: string
+  dropLineF: string
+}
 
 interface ReportCell {
   report: GTFS_RT_Static_Report_Metadata_JSON | null
@@ -51,6 +67,7 @@ interface ReportCell {
   error: string | null
   dayCell: DayCell,
   hourCell: HourCell,
+  compareMetadata: GTFS_RT_StaticReportCompareMetadata | null,
 }
 
 interface PageModel {
