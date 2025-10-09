8a774963333ee515dc624d3a65f726caa78127bf
Vasile Cotovanu <vasile.cotovanu@gmail.com>
2025-10-09 22:59:35 +0200

    Adds symlink calls

--- tools/scripts/cli_otd_fetch_bo.sh
+++ tools/scripts/cli_otd_fetch_bo.sh
@@ -9,8 +9,10 @@ source $PYTHON_VENV_PATH/bin/activate
 
 BO_LOGFILE=$LOGS_BASEPATH/otd_fetch_csv_business-organisations-$DATE_NOW.log
 python3 $DIR/cli_otd_fetch_csv_package.py --package_id business-organisations 2>&1 | tee $BO_LOGFILE
+python3 $DIR/cli_otd_symlink_package.py --package_id business-organisations 2>&1 | tee -a $BO_LOGFILE
 symlink_latest $BO_LOGFILE
 
 GO_LOGFILE=$LOGS_BASEPATH/otd_fetch_csv_go-realtime-$DATE_NOW.log
 python3 $DIR/cli_otd_fetch_csv_package.py --package_id go-realtime 2>&1 | tee $GO_LOGFILE
+python3 $DIR/cli_otd_symlink_package.py --package_id go-realtime 2>&1 | tee -a $GO_LOGFILE
 symlink_latest $GO_LOGFILE
