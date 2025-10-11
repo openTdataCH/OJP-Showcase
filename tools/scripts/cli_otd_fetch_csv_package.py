d8023bc908ffa2b7975844e94b403ebd2c720b3b
Vasile Cotovanu <vasile.cotovanu@gmail.com>
2025-10-11 23:24:55 +0200

    Allow to overwrite

--- tools/scripts/cli_otd_fetch_csv_package.py
+++ tools/scripts/cli_otd_fetch_csv_package.py
@@ -16,6 +16,7 @@ def main():
     
     parser = argparse.ArgumentParser()
     parser.add_argument('--package_id', '--package_id')
+    parser.add_argument('--overwrite', '--overwrite')
     args = parser.parse_args()
     
     package_id = args.package_id
@@ -33,6 +34,8 @@ def main():
         print(f"possible: values: {', '.join(package_ids)}" )
         sys.exit(1)
         
+    overwrite = args.overwrite in ['true', 'yes', '1']
+        
     package_config = app_config['csv_latest_data'][package_id]
     package_info_url = package_config['info_url']
     
@@ -43,14 +46,14 @@ def main():
     print(f'  - {package_info_url}')
     print()
     
-    _run_package(script_path, app_config, package_id)
+    _run_package(script_path, app_config, package_id, overwrite)
     
     print()
     print('... DONE')
     
-def _run_package(script_path: Path, app_config: Any, package_id: str):
+def _run_package(script_path: Path, app_config: Any, package_id: str, overwrite: bool):
     _fetch_metadata(script_path, package_id)
-    _fetch_resource(script_path, app_config, package_id)
+    _fetch_resource(script_path, app_config, package_id, overwrite)
     
 def _fetch_metadata(script_path, package_id: str):
     ckan_fetch_cli_path = f'{script_path.parent}/../ckan-utils/fetch_metadata_cli.py'
@@ -63,31 +66,35 @@ def _fetch_metadata(script_path, package_id: str):
     
     print()
     
-def _fetch_resource(script_path: Path, app_config, package_id: str):
+def _fetch_resource(script_path: Path, app_config, package_id: str, overwrite: bool):
     file_prefix_config = app_config['csv_latest_data'][package_id].get('file_prefix', None)
     if file_prefix_config is None:
-        _fetch_latest_resource(script_path, package_id)
+        _fetch_latest_resource(script_path, package_id, overwrite)
     else:
         file_prefixes = file_prefix_config if isinstance(file_prefix_config, list) else [file_prefix_config]
-        _fetch_resource_by_prefix(app_config, script_path, package_id, file_prefixes)
+        _fetch_resource_by_prefix(app_config, script_path, package_id, file_prefixes, overwrite)
     #
 
-def _fetch_latest_resource(script_path: Path, package_id: str):
+def _fetch_latest_resource(script_path: Path, package_id: str, overwrite: bool):
+    overwrite_s = 'yes' if overwrite else 'no'
+    
     # fetch latest archive
     ckan_fetch_cli_path = f'{script_path.parent}/../ckan-utils/fetch_package_cli.py'
-    ckan_fetch_sh = f'{PYTHON_PATH} {ckan_fetch_cli_path} --package_id {package_id}'
+    ckan_fetch_sh = f'{PYTHON_PATH} {ckan_fetch_cli_path} --package_id {package_id} --overwrite {overwrite_s}'
     
     print(f'STEP {package_id}.2 - FETCH LATEST RESOURCE')
     print(ckan_fetch_sh, flush=True)
     os.system(ckan_fetch_sh)
     
-def _fetch_resource_by_prefix(app_config: Any, script_path: Path, package_id: str, resource_prefixes: List[str]):
+def _fetch_resource_by_prefix(app_config: Any, script_path: Path, package_id: str, resource_prefixes: List[str], overwrite: bool):
+    overwrite_s = 'yes' if overwrite else 'no'
+    
     for resource_prefix in resource_prefixes:
         ckan_resource = compute_ckan_resource_by_prefix(app_config, package_id, resource_prefix)
     
         # fetch latest archive
         ckan_fetch_cli_path = f'{script_path.parent}/../ckan-utils/fetch_package_cli.py'
-        ckan_fetch_sh = f'{PYTHON_PATH} {ckan_fetch_cli_path} --package_id {package_id} --resource_title {ckan_resource.identifier}'
+        ckan_fetch_sh = f'{PYTHON_PATH} {ckan_fetch_cli_path} --package_id {package_id} --resource_title {ckan_resource.identifier} --overwrite {overwrite_s}'
     
         print(f'STEP {package_id}.2 - FETCH RESOURCE by PREFIX {ckan_resource.identifier}')
         print(ckan_fetch_sh, flush=True)
