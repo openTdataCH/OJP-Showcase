8e02a818de8f10a5d0a13b5124695200725f050a
Vasile Cotovanu <vasile.cotovanu@gmail.com>
2026-03-03 11:03:30 +0100

    Use shared bootstrap

--- tools/hrdf-check-duplicates/bootstrap.sh
+++ tools/hrdf-check-duplicates/bootstrap.sh
@@ -1,12 +1,1 @@
-#!/usr/bin/env bash
-set -e
-
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-
-if [ ! -d "$DIR/.venv" ]; then
-  python3 -m venv $DIR/.venv
-fi
-
-source $DIR/.venv/bin/activate
-pip install --upgrade pip
-pip install -r $DIR/requirements.txt
+../_shared/bootstrap.sh
