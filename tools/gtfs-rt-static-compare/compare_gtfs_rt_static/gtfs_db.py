741b685c3aa86f5c7208ac64b557c7238ed7e192
Vasile Cotovanu <vasile.cotovanu@gmail.com>
2026-03-04 16:34:30 +0100

    No need for Optional

--- tools/gtfs-rt-static-compare/compare_gtfs_rt_static/gtfs_db.py
+++ tools/gtfs-rt-static-compare/compare_gtfs_rt_static/gtfs_db.py
@@ -40,7 +40,7 @@ class GTFS_DB:
             # Dont use Trip because is slower init
             # self.map_trips[trip_id] = Trip.init_from_db_row(trip_db_json, gtfs_calendar_db, gtfs_agency_db, gtfs_routes_db, gtfs_stops_db)
         
-    def _load_gtfs_table(self, table_name: str, map_by_field: str = None):
+    def _load_gtfs_table(self, table_name: str, map_by_field: str):
         res_cache_path = None
         if self._gtfs_db_table_cache_template is not None:
             res_cache_path_s: str = f'{self._gtfs_db_table_cache_template}'
