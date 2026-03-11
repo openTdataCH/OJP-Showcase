604a30c5a368c7621326f5c57433a5f9e1188cd3
Vasile Cotovanu <vasile.cotovanu@gmail.com>
2026-03-11 11:18:08 +0100

    Order

--- apps/gtfs-rt-static-report/src/app/app.module.ts
+++ apps/gtfs-rt-static-report/src/app/app.module.ts
@@ -1,16 +1,15 @@
 import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
-
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
 
 import { HttpClientModule } from '@angular/common/http';
 
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { AppRoutingModule } from './app-routing.module';
 
+import { AppComponent } from './app.component';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
     BrowserModule,
