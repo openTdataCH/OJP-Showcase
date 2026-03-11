0f6b088e2705efc3ca9912626b44ec8ac0759d21
Vasile Cotovanu <vasile.cotovanu@gmail.com>
2026-03-11 11:18:45 +0100

    Registers Home Component

--- apps/gtfs-rt-static-report/src/app/app.module.ts
+++ apps/gtfs-rt-static-report/src/app/app.module.ts
@@ -7,9 +7,11 @@ import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home.component';
 @NgModule({
   declarations: [
     AppComponent,
+    HomeComponent,
   ],
   imports: [
     BrowserModule,
