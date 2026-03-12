b14151ac94b5aa7608929e03b6fb594e2017b128
Vasile Cotovanu <vasile.cotovanu@gmail.com>
2026-03-12 17:43:21 +0100

    Adds helper to format dates

--- apps/gtfs-rt-static-report/src/app/helpers/date-helpers.ts
+++ apps/gtfs-rt-static-report/src/app/helpers/date-helpers.ts
@@ -34,4 +34,19 @@ export class DateHelpers {
       return false;
     }
   }
+
+  public static formatDate(date: Date = new Date()): string {
+    const yyyy = date.getFullYear();
+
+    const mm = String(date.getMonth() + 1).padStart(2, '0');
+    const dd = String(date.getDate()).padStart(2, '0');
+
+    const hh = String(date.getHours()).padStart(2, '0');
+    const min = String(date.getMinutes()).padStart(2, '0');
+    const ss = String(date.getSeconds()).padStart(2, '0');
+
+    const result = `${yyyy}-${mm}-${dd} ${hh}:${min}:${ss}`;
+
+    return result;
+  }
 }
