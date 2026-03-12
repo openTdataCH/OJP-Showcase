51c6f08c965e9089d54d5683089a0502e2a755be
Vasile Cotovanu <vasile.cotovanu@gmail.com>
2026-03-12 14:37:10 +0100

    Use children approach

--- apps/gtfs-rt-static-report/src/app/app-routing.module.ts
+++ apps/gtfs-rt-static-report/src/app/app-routing.module.ts
@@ -2,10 +2,17 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 import { HomeComponent } from './home.component';
+import { AppComponent } from './app.component';
 
 const routes: Routes = [
-  { path: 'home', component: HomeComponent },
-  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  {
+    path: '',
+    component: AppComponent,
+    children: [
+      { path: 'home', component: HomeComponent },
+      { path: '', redirectTo: '/home', pathMatch: 'full' },
+    ]
+  }
 ];
 
 @NgModule({
