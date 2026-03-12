29ebdcf45e93d9a8b9afcd0a704127b5ccfd6c1b
Vasile Cotovanu <vasile.cotovanu@gmail.com>
2026-03-12 17:46:08 +0100

    Adds GTFS-RT fetch and gzip parsing via pako

--- apps/gtfs-rt-static-report/src/app/data.service.ts
+++ apps/gtfs-rt-static-report/src/app/data.service.ts
@@ -2,7 +2,10 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { firstValueFrom } from 'rxjs';
 
-import { GTFS_RT_Static_Monthly_Report_JSON } from './app.component';
+import * as pako from 'pako';
+
+import { GTFS_RT_Static_Monthly_Report_JSON } from './home.component';
+import { Response_GTFS_RT } from './models/gtfs-rt/gtfs-rt-response';
 
 @Injectable({
   providedIn: 'root'
@@ -21,5 +24,21 @@ export class DataService {
     url = url.replace('[YYYY-MM]', dateYM);
     
     return await firstValueFrom(this.http.get<GTFS_RT_Static_Monthly_Report_JSON>(url));
+  }
+
+  public async fetchGzipJson<T>(url: string): Promise<T> {
+    const compressedBuffer = await firstValueFrom(
+      this.http.get(url, {
+        responseType: 'arraybuffer',
+      })
+    );
+
+    const jsonText = pako.ungzip(
+      new Uint8Array(compressedBuffer),
+      { to: 'string' }
+    );
+
+    const result = JSON.parse(jsonText) as T;
+    return result;
   }
 }
