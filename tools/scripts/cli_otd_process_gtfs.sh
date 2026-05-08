323f570ce0b3a1a16d218e487e774d9042ce90fb
Vasile Cotovanu <vasile.cotovanu@gmail.com>
2026-05-08 15:15:42 +0200

    No need to activate, use pyenv paths directly

--- tools/scripts/cli_otd_process_gtfs.sh
+++ tools/scripts/cli_otd_process_gtfs.sh
@@ -5,8 +5,9 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 DATE_NOW=$(date +"%Y-%m-%d")
 
 source $DIR/common.sh
-source $PYTHON_VENV_PATH/bin/activate
 
 LOGFILE=$LOGS_BASEPATH/otd_process-gtfs-$DATE_NOW.log
-python3 $DIR/gtfs-compute-latest.py 2>&1 | tee $LOGFILE
+PYTHON_PATH=$DIR/.venv/bin/python3
+
+$PYTHON_PATH $DIR/gtfs-compute-latest.py 2>&1 | tee $LOGFILE
 symlink_latest $LOGFILE
