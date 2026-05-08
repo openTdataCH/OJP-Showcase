5046a359b1286384ccecadae73d7b5c11fdb4a60
Vasile Cotovanu <vasile.cotovanu@gmail.com>
2026-05-08 19:18:59 +0200

    new URL

--- apps/_shared/ts/types/gtfs/gtfs.ts
+++ apps/_shared/ts/types/gtfs/gtfs.ts
@@ -103,7 +103,7 @@ export interface GTFS_DB_Trips_Response {
     rows: GTFS_Static_Trip_Condensed[],
 }
 
-// https://tools.odpch.ch/gtfs-query/trip/1.TA.96-702-j26-1.1.H
+// https://tools.opentransportdata.swiss/gtfs-query/trip/1.TA.96-702-j26-1.1.H
 export interface TripDetailResponseJSON {
     message: string[]
     result: {
