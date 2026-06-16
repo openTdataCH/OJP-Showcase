62f61b94b6ab30c9e96f76d855de9fd32522bc0e
Vasile Cotovanu <vasile.cotovanu@gmail.com>
2026-06-16 14:53:46 +0200

    Use also link_agency

--- tools/_shared/inc/sql/gtfs/query_day_trips.sql
+++ tools/_shared/inc/sql/gtfs/query_day_trips.sql
@@ -1,10 +1,11 @@
 SELECT 
     [SQL_FIELDS]
 FROM 
-    trips, calendar, routes, agency
+    trips, calendar, routes, agency, link_agency
 WHERE 
     trips.service_id = calendar.service_id 
     AND trips.route_id = routes.route_id
     AND routes.agency_id = agency.agency_id
+    AND routes.agency_id = link_agency.agency_id
 
     [EXTRA_WHERE]
