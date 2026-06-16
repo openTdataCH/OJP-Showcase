491555ef2d2933641bdd7e43a71e4633a0dffd11
Vasile Cotovanu <vasile.cotovanu@gmail.com>
2026-06-16 14:52:15 +0200

    Adds agency models and DB typedDicts

---
+++ tools/_shared/inc/models/gtfs/route.py
@@ -0,0 +1,39 @@
+import os, sys
+
+from dataclasses import dataclass
+from typing import TypedDict, Union
+
+from .agency import Agency
+
+class RouteDB(TypedDict):
+    route_id: str
+    agency_id: str
+    route_short_name: str
+    route_desc: Union[str, None]
+    route_type: int
+    day_bits: str
+
+@dataclass(slots=True)
+class Route:
+    id: str
+    agency: Agency
+    route_short_name: str
+    route_type: int
+    route_desc: Union[str, None] = None
+
+    @classmethod
+    def from_db_row(cls, row: RouteDB, map_agency: dict[str, Agency]) -> Route:
+        agency_id = row['agency_id']
+        if agency_id not in map_agency:
+            raise ValueError(f'cant find {agency_id} in map_agency')
+        agency = map_agency[agency_id]
+
+        route = Route(
+            id=row['route_id'],
+            agency=agency,
+            route_short_name=row['route_short_name'],
+            route_type=row['route_type'],
+            route_desc=row['route_desc'],
+        )
+
+        return route
