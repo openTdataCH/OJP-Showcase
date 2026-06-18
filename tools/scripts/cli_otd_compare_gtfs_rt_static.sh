f31cec7594857923213cc89cb29ed53faf9cf243
Vasile Cotovanu <vasile.cotovanu@gmail.com>
2026-06-18 14:20:20 +0200

    Handle mail in cron

--- tools/scripts/cli_otd_compare_gtfs_rt_static.sh
+++ tools/scripts/cli_otd_compare_gtfs_rt_static.sh
@@ -11,36 +11,12 @@ PYTHON_PATH=$TOOL_FOLDER_PATH/.venv/bin/python3
 
 LOGFILE=$LOGS_BASEPATH/otd_compare_gtfs_rt_static-$DATE_NOW.log
 touch "$LOGFILE"
-
-# '|| :' at end is to ignore failures
-echo "Step 1/2: running cli_compare_latest.py ..."
-$PYTHON_PATH $TOOL_FOLDER_PATH/cli_compare_latest.py >>"$LOGFILE" 2>>"$LOGFILE" || :
-echo ""
-
-status=0
-echo "Step 2/2: running cli_aggregate_monthly_reports_json.py ..."
-$PYTHON_PATH $TOOL_FOLDER_PATH/cli_aggregate_monthly_reports_json.py >>"$LOGFILE" 2>>"$LOGFILE" || status=$?
-echo ""
 symlink_latest $LOGFILE
 
-DETAIL_REPORT_DATE_F=$(grep -Po 'report-\K[0-9]{4}-[0-9]{2}-[0-9]{2}-[0-9]{4}(?=\.json)' $LOGFILE)
-DETAIL_REPORT_URL=https://tools.opentransportdata.swiss/gtfs-rt-static-report/detail/$DETAIL_REPORT_DATE_F
+echo "Step 1/2: running cli_compare_latest.py ..."
+$PYTHON_PATH $TOOL_FOLDER_PATH/cli_compare_latest.py >>"$LOGFILE"
+echo ""
 
-if [ "$status" -ne 0 ]; then
-  {
-    echo "From: $DEFAULT_MAIL_FROM"
-    echo "To: $GTFS_RT_STATIC_COMPARE_MAIL_TO"
-    echo "Subject: GTFS-RT -static compare issue"
-    echo "Content-Type: text/plain; charset=UTF-8"
-    echo
-    echo "Report: https://tools.opentransportdata.swiss/gtfs-rt-static-report/"
-    echo "Log: https://tools.opentransportdata.swiss/tmp/logs/otd_compare_gtfs_rt_static-LATEST.log"
-    echo "Detail by agency: $DETAIL_REPORT_URL"
-    echo
-    echo "-> last 50 rows"
-    echo
-    echo " ....................................................... "
-    tail -n 50 "$LOGFILE"
-  } | /usr/sbin/sendmail -t
-  exit "$status"
-fi
+echo "Step 2/2: running cli_aggregate_monthly_reports_json.py ..."
+$PYTHON_PATH $TOOL_FOLDER_PATH/cli_aggregate_monthly_reports_json.py --check-errors >>"$LOGFILE"
+echo ""
